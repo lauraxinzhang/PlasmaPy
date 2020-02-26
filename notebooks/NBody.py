2951b3e090ac085dab5101db4916d6f4348bdaf4
Dominik Stańczak <stanczakdominik@gmail.com>
2020-02-26 19:43:34 +0100

    Schtuff

--- notebooks/NBody.py
+++ notebooks/NBody.py
@@ -25,19 +25,26 @@ from plasmapy.classes.sources.interparticle import InterParticleForces
 
 
 eq_distance = 0.1
-forces = {key: InterParticleForces(key, 100, eq_distance, 1, 1e3, 0.01, 7) for key in ('python', 'njit', 'njit_parallel')}
+forces = {
+    key: InterParticleForces(key, 100, eq_distance, 1, 1e3, 0.01, 7)
+    for key in ("python", "njit", "njit_parallel")
+}
 
 from collections import namedtuple
-CustomParticle = namedtuple('custom_particle', ['mass', 'charge'])
-particle = CustomParticle(mass=1 * u.dimensionless_unscaled, charge=1 * u.dimensionless_unscaled)
+
+CustomParticle = namedtuple("custom_particle", ["mass", "charge"])
+particle = CustomParticle(
+    mass=1 * u.dimensionless_unscaled, charge=1 * u.dimensionless_unscaled
+)
 
 L = 1 * u.m
 N = 128
 np.random.seed(0)
-x = u.Quantity(np.random.random((N, 3))*L,  u.m)
+x = u.Quantity(np.random.random((N, 3)) * L, u.m)
 v = u.Quantity(np.zeros(x.shape, dtype=float), u.m / u.s)
 
 from scipy import spatial
+
 tree = spatial.cKDTree(x)
 close_pairs = tree.query_pairs(eq_distance)
 while close_pairs:
@@ -50,4 +57,9 @@ while close_pairs:
 # In[ ]:
 
 
-solutions = {engine: simulation.ParticleTracker(forces[engine], x, v, particle).run(1e-3 * u.s, dt = 1e-6 * u.s) for engine in ['njit', 'njit_parallel']}
+solutions = {
+    engine: simulation.ParticleTracker(forces[engine], x, v, particle).run(
+        1e-3 * u.s, dt=1e-6 * u.s
+    )
+    for engine in ["njit", "njit_parallel"]
+}
